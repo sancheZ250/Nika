a72db1c4d9c5d906e1dd3ade2b741c8038c154b3
sancheZ250 <sancheZ2502@yandex.ru>
2023-08-17 14:11:29 +0800

    changed database from sqlite3 to PostgreSQL

--- Nika2023/Nika2023/settings.py
+++ Nika2023/Nika2023/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'Nika2023.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Nika',
+        'USER': 'postgres',
+        'PASSWORD': 'qweewqqwe',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
